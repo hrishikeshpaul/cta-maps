c28f51488db48aad2eefd672fcb25f684fd53352
Hrishikesh <hrishikeshpaul12@gmail.com>
2021-12-25 22:02:07 -0600

    chore: added basic web

---
+++ web-client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Button } from "react-bootstrap";
+
+import "./App.scss";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Button>Click Me</Button>
+    </div>
+  );
+};
