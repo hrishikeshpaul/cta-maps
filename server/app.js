4e434bad63ee2b311cb77dc07027dfe1d9841fd2
Hrishikesh <hrishikeshpaul12@gmail.com>
2021-12-26 18:10:21 -0600

    chore: updated

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import dotenv from "dotenv";
+import chalk from "chalk";
 import cors from "cors";
 import express from "express";
 
@@ -13,7 +14,10 @@ app.use(cors());
 
 app.use("/v1/api", router);
 
-app.listen(process.env.PORT, () => {
-  // console.log(process.env.CTA_KEY);
-  console.log("Server started on port", process.env.PORT);
-});
+if (process.env.CTA_KEY) {
+  app.listen(process.env.PORT, () => {
+    console.log("Server started on port", process.env.PORT);
+  });
+} else {
+  console.log(chalk.red("CTA key missing. Failed to start server"));
+}
