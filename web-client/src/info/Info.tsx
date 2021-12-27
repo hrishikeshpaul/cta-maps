ae930dfb03be5a12f236fb5a46871a783ae9753a
Hrishikesh <hrishikeshpaul12@gmail.com>
2021-12-26 18:49:36 -0600

    chore: updated info component

--- web-client/src/info/Info.tsx
+++ web-client/src/info/Info.tsx
@@ -2,21 +2,35 @@ import React, { FunctionComponent } from 'react';
 
 import {
     Avatar,
+    Box,
     Drawer,
     DrawerBody,
     DrawerHeader,
     DrawerOverlay,
     DrawerContent,
+    DrawerFooter,
     Text,
     IconButton,
     Flex,
+    Link,
 } from '@chakra-ui/react';
+import { FiChevronRight } from 'react-icons/fi';
 import { IoIosClose } from 'react-icons/io';
 
 import { useStore } from '../store/Store';
 
+import './Info.scss';
+
 export const Info: FunctionComponent = () => {
     const [{ infoOpen }, { closeInfo }] = useStore();
+
+    const onBugReport = () => {
+        window.open('https://github.com/hrishikeshpaul/cta-maps/issues/new', '_blank');
+    };
+
+    const onContribute = () => {
+        window.open('https://github.com/hrishikeshpaul/cta-maps/', '_blank');
+    };
     return (
         <Drawer isOpen={infoOpen} placement="right" size="md" onClose={closeInfo} autoFocus={false}>
             <DrawerOverlay />
@@ -35,18 +49,43 @@ export const Info: FunctionComponent = () => {
                     </Flex>
                 </DrawerHeader>
 
-                <DrawerBody px="4" d="flex" flexDir="column" alignItems="center" pt="0">
-                    <Avatar src="/logo.svg" size="xl" />
-                    <Text fontSize="lg" fontWeight="bold" pt="2">
-                        CTA Maps
-                    </Text>
-                    <Text color="gray.500" textAlign="center" fontSize="xs">
-                        v1.0.0
-                    </Text>
-                    <Text color="gray.700" textAlign="center">
-                        An application to track CTA busses, so that you are not waiting at the bus stop forever.
-                    </Text>
+                <DrawerBody px="4" pt="0" className="info">
+                    <Flex flexDir="column" alignItems="center">
+                        <Avatar src="/logo.svg" size="xl" />
+                        <Text fontSize="lg" fontWeight="bold" pt="2">
+                            CTA Maps
+                        </Text>
+                        <Text color="gray.400" textAlign="center" fontSize="xs">
+                            v1.0.0
+                        </Text>
+                        <Text color="gray.600" textAlign="center">
+                            An application to track CTA busses, so that you are not waiting at the bus stop forever.
+                        </Text>
+                    </Flex>
+                    <Box mt="8" className="info-box">
+                        <Flex p="4" className="item">
+                            <Text fontWeight="medium">Usage Manual</Text>
+                            <FiChevronRight />
+                        </Flex>
+                        <Flex p="4" className="item">
+                            <Text fontWeight="medium">Frequently Asked Questions</Text>
+                            <FiChevronRight />
+                        </Flex>
+                        <Flex p="4" className="item" onClick={onBugReport}>
+                            <Text fontWeight="medium">Report Bug</Text>
+                            <FiChevronRight />
+                        </Flex>
+                    </Box>
                 </DrawerBody>
+                <DrawerFooter justifyContent="flex-start">
+                    <Text fontSize="sm">
+                        Want to contribute? Click{' '}
+                        <Link fontWeight="bold" color="blue.500" onClick={onContribute}>
+                            here
+                        </Link>
+                        .
+                    </Text>
+                </DrawerFooter>
             </DrawerContent>
         </Drawer>
     );
