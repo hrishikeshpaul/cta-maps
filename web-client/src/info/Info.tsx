a0fd9cb87c8422e3f129f14d91d7b93f276f20a6
Hrishikesh <hrishikeshpaul12@gmail.com>
2022-01-04 11:41:56 -0600

    chore: updated name

--- web-client/src/info/Info.tsx
+++ web-client/src/info/Info.tsx
@@ -61,7 +61,7 @@ export const Info: FunctionComponent = () => {
             <DrawerContent>
                 <DrawerHeader px="4">
                     <Flex justifyContent="space-between" alignItems="center">
-                        <Text fontWeight="bold">TrackCTA</Text>
+                        <Text fontWeight="bold">trackCTA</Text>
                         <IconButton
                             variant="ghost"
                             fontSize="3xl"
