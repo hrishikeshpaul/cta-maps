2b8d90297d8f138287adae07d44a709660112d36
Hrishikesh <hrishikeshpaul12@gmail.com>
2022-01-06 19:09:07 -0600

    chore: added font

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet } from 'react-native';
 
-import { NativeBaseProvider, Box } from 'native-base';
+import { NativeBaseProvider, Box, extendTheme } from 'native-base';
+import { useFonts } from 'expo-font';
 
 import { Map } from './src/map/Map';
 import { DataStoreProvider } from './src/store/data/DataStore';
@@ -8,9 +9,67 @@ import { SystemStoreProvider } from './src/store/system/SystemStore';
 import { Nav } from './src/nav/Nav';
 import { RouteSelect } from './src/route-select/RouteSelect';
 
+const theme = extendTheme({
+    fontConfig: {
+        Inter: {
+            100: {
+                normal: 'Inter-Light',
+                italic: 'Inter-LightItalic',
+            },
+            200: {
+                normal: 'Inter-Light',
+                italic: 'Inter-LightItalic',
+            },
+            300: {
+                normal: 'Inter-Light',
+                italic: 'Inter-LightItalic',
+            },
+            400: {
+                normal: 'Inter-Regular',
+                italic: 'Inter-Italic',
+            },
+            500: {
+                normal: 'Inter-Medium',
+            },
+            600: {
+                normal: 'Inter-Medium',
+                italic: 'Inter-MediumItalic',
+            },
+            700: {
+                normal: 'Inter-SemiBold',
+            },
+            800: {
+                normal: 'Inter-Bold',
+                italic: 'Inter-BoldItalic',
+            },
+            900: {
+                normal: 'Inter-Bold',
+                italic: 'Inter-BoldItalic',
+            },
+        },
+    },
+
+    fonts: {
+        heading: 'Inter',
+        body: 'Inter',
+        mono: 'Inter',
+    },
+});
+
 export default function App() {
+    const [loaded] = useFonts({
+        'Inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
+        'Inter-Medium': require('./assets/fonts/Inter-Medium.ttf'),
+        'Inter-SemiBold': require('./assets/fonts/Inter-SemiBold.ttf'),
+        'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+    });
+
+    if (!loaded) {
+        return null;
+    }
+
     return (
-        <NativeBaseProvider>
+        <NativeBaseProvider theme={theme}>
             <SystemStoreProvider>
                 <DataStoreProvider>
                     <Box style={styles.container}>
