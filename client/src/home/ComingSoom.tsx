a6679f7a1d340003f0ef87203a822e9ca34be22e
Hrishikesh <hrishikeshpaul12@gmail.com>
2022-01-11 17:30:34 -0600

    chore: updated coming soon

--- client/src/home/ComingSoom.tsx
+++ client/src/home/ComingSoom.tsx
@@ -1,15 +1,16 @@
 import { FC } from 'react';
 
-import { Box, Container, Text } from '@chakra-ui/react';
+import { Box, Container, Text, useColorModeValue } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 
 export const ComingSoon: FC = () => {
     const { t } = useTranslation();
+    const bg = useColorModeValue('gray.50', 'gray.700');
 
     return (
-        <Box py="24" textAlign="center">
+        <Box bg={bg} py="24" textAlign="center">
             <Container maxW="container.lg">
-                <Text fontWeight="900" fontSize={{ base: '3xl', md: '4xl' }} color="red.500">
+                <Text fontWeight="900" fontSize={{ base: '3xl', md: '4xl' }}>
                     {t('COMING_SOON')}
                 </Text>
             </Container>
