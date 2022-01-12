aa9258bc0a302b78b4e82b87d146928341edc42f
Hrishikesh <hrishikeshpaul12@gmail.com>
2022-01-12 12:07:53 -0600

    chore: monior refactors

--- client/src/home/Home.tsx
+++ client/src/home/Home.tsx
@@ -2,8 +2,8 @@ import { FC } from 'react';
 
 import { Box, Button, Center, Container, Heading, Text, useColorModeValue } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
-import { ComingSoon } from 'home/ComingSoon';
 
+import { ComingSoon } from 'home/ComingSoon';
 import { Features } from 'home/Features';
 import { Footer } from 'shared/Footer';
 
