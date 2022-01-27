a9a14a49b4f6c03cc06d156fe3d16d6c19e2e146
Hrishikesh <hrishikeshpaul12@gmail.com>
2022-01-27 14:47:01 -0600

    chore: added base page

--- web-app/src/saved/Saved.tsx
+++ web-app/src/saved/Saved.tsx
@@ -21,6 +21,7 @@ import { useDataStore } from 'store/data/DataStore';
 import { CheckIcon, RightIcon } from 'utils/Icons';
 import { FavoriteIcon } from 'shared/favorite-icon/FavoriteIcon';
 import { Route } from 'store/data/DataStore.Types';
+import { BasePage } from 'utils/BasePage';
 
 export const Saved: FunctionComponent = () => {
     const { t } = useTranslation();
@@ -37,199 +38,77 @@ export const Saved: FunctionComponent = () => {
     };
 
     return (
-        <>
-            <Container maxW="container.sm" py="4" px="0">
-                <Text fontWeight="bold" fontSize="4xl" px="4">
-                    {t('SAVED')}
-                </Text>
-                <Tabs isFitted my="4">
-                    <TabList mx="4">
-                        <Tab fontWeight="600">{t('STOPS')}</Tab>
-                        <Tab fontWeight="600">{t('ROUTES')}</Tab>
-                    </TabList>
-                    <TabPanels p="0">
-                        <TabPanel px="0">
-                            {Object.values(favoriteStops).length === 0 && <Text px="4">{t('ADD_FAVORITES')}</Text>}
-                            {Object.values(favoriteStops).map((favorite) => {
-                                return (
-                                    <Box
-                                        key={`fav-stops-${favorite.id}`}
-                                        _active={{ bg }}
-                                        onClick={() => openStop(favorite)}
-                                    >
+        <BasePage title={t('SAVED')}>
+            <Tabs isFitted my="4">
+                <TabList mx="4">
+                    <Tab fontWeight="600">{t('STOPS')}</Tab>
+                    <Tab fontWeight="600">{t('ROUTES')}</Tab>
+                </TabList>
+                <TabPanels p="0">
+                    <TabPanel px="0">
+                        {Object.values(favoriteStops).length === 0 && <Text px="4">{t('ADD_FAVORITES')}</Text>}
+                        {Object.values(favoriteStops).map((favorite) => {
+                            return (
+                                <Box
+                                    key={`fav-stops-${favorite.id}`}
+                                    _active={{ bg }}
+                                    onClick={() => openStop(favorite)}
+                                >
+                                    <Flex p="4" alignItems="center" justifyContent="space-between" overflow="hidden">
+                                        <Text fontWeight="600" isTruncated>
+                                            {favorite.name}
+                                        </Text>
+                                        <Box pl="2">
+                                            <RightIcon />
+                                        </Box>
+                                    </Flex>
+                                    <Divider />
+                                </Box>
+                            );
+                        })}
+                    </TabPanel>
+                    <TabPanel px="0">
+                        {Object.values(favoriteRoutes).length === 0 && <Text px="4">{t('ADD_FAVORITES')}</Text>}
+                        {Object.values(favoriteRoutes).map(({ route, color, name }) => {
+                            return (
+                                <Box key={`fav-route-${route}`} _active={{ bg }}>
+                                    <Flex justifyContent="center" alignItems="center" p="4">
                                         <Flex
-                                            p="4"
                                             alignItems="center"
-                                            justifyContent="space-between"
                                             overflow="hidden"
+                                            w="100%"
+                                            onClick={() => onToggleRoute({ route, color, name })}
                                         >
-                                            <Text fontWeight="600" isTruncated>
-                                                {favorite.name}
-                                            </Text>
-                                            <Box pl="2">
-                                                <RightIcon />
-                                            </Box>
-                                        </Flex>
-                                        <Divider />
-                                    </Box>
-                                );
-                            })}
-                        </TabPanel>
-                        <TabPanel px="0">
-                            {Object.values(favoriteRoutes).length === 0 && <Text px="4">{t('ADD_FAVORITES')}</Text>}
-                            {Object.values(favoriteRoutes).map(({ route, color, name }) => {
-                                return (
-                                    <Box key={`fav-route-${route}`} _active={{ bg }}>
-                                        <Flex justifyContent="center" alignItems="center" p="4">
-                                            <Flex
-                                                alignItems="center"
-                                                overflow="hidden"
-                                                w="100%"
-                                                onClick={() => onToggleRoute({ route, color, name })}
-                                            >
-                                                <Center h="40px" w="40px" bg={color} borderRadius="md">
-                                                    <Text color="white" fontWeight="bold">
-                                                        {route}
-                                                    </Text>
-                                                </Center>
-                                                <Text px="4" isTruncated fontWeight={500}>
-                                                    {name}
+                                            <Center h="40px" w="40px" bg={color} borderRadius="md">
+                                                <Text color="white" fontWeight="bold">
+                                                    {route}
                                                 </Text>
-                                            </Flex>
-                                            <Flex alignItems="center" pl="2">
-                                                {routes[route] && (
-                                                    <Icon fontSize="2xl" mr="3">
-                                                        <CheckIcon />
-                                                    </Icon>
-                                                )}
-
-                                                <FavoriteIcon
-                                                    ariaLabel="fav-route"
-                                                    onClick={() => unSaveRoute(route)}
-                                                    isFav
-                                                />
-                                            </Flex>
+                                            </Center>
+                                            <Text px="4" isTruncated fontWeight={500}>
+                                                {name}
+                                            </Text>
                                         </Flex>
-                                        <Divider />
-                                    </Box>
-                                );
-                            })}
-                        </TabPanel>
-                    </TabPanels>
-                </Tabs>
-            </Container>
-            {/* <Drawer
-                isOpen={favoritesOpen}
-                placement="right"
-                size="md"
-                onClose={closeFavorites}
-                autoFocus={false}
-                closeOnOverlayClick
-                id="favoriteStops"
-            >
-                <DrawerOverlay onClick={closeFavorites} />
-                <DrawerContent position="absolute">
-                    <DrawerHeader px="4">
-                        <Flex justifyContent="space-between" alignItems="center">
-                            <Text fontWeight="bold" fontSize="2xl">
-                                {t('FAVORITES')}
-                            </Text>
-                            <IconButton
-                                variant="ghost"
-                                fontSize="3xl"
-                                aria-label="close"
-                                mr="-3"
-                                onClick={closeFavorites}
-                                icon={<CloseIcon />}
-                            />
-                        </Flex>
-                    </DrawerHeader>
+                                        <Flex alignItems="center" pl="2">
+                                            {routes[route] && (
+                                                <Icon fontSize="2xl" mr="3">
+                                                    <CheckIcon />
+                                                </Icon>
+                                            )}
 
-                    <DrawerBody px="0" pt="0">
-                        <Tabs isFitted>
-                            <TabList mx="4">
-                                <Tab fontWeight="600">{t('STOPS')}</Tab>
-                                <Tab fontWeight="600">{t('ROUTES')}</Tab>
-                            </TabList>
-                            <TabPanels p="0">
-                                <TabPanel px="0">
-                                    {Object.values(favoriteStops).length === 0 && (
-                                        <Text px="4">{t('ADD_FAVORITES')}</Text>
-                                    )}
-                                    {Object.values(favoriteStops).map((favorite) => {
-                                        return (
-                                            <Box
-                                                key={`fav-stops-${favorite.id}`}
-                                                _active={{ bg }}
-                                                onClick={() => openStop(favorite)}
-                                            >
-                                                <Flex
-                                                    p="4"
-                                                    alignItems="center"
-                                                    justifyContent="space-between"
-                                                    overflow="hidden"
-                                                >
-                                                    <Text fontWeight="600" isTruncated>
-                                                        {favorite.name}
-                                                    </Text>
-                                                    <Box pl="2">
-                                                        <RightIcon />
-                                                    </Box>
-                                                </Flex>
-                                                <Divider />
-                                            </Box>
-                                        );
-                                    })}
-                                </TabPanel>
-                                <TabPanel px="0">
-                                    {Object.values(favoriteRoutes).length === 0 && (
-                                        <Text px="4">{t('ADD_FAVORITES')}</Text>
-                                    )}
-                                    {Object.values(favoriteRoutes).map(({ route, color, name }) => {
-                                        return (
-                                            <Box key={`fav-route-${route}`} _active={{ bg }}>
-                                                <Flex justifyContent="center" alignItems="center" p="4">
-                                                    <Flex
-                                                        alignItems="center"
-                                                        overflow="hidden"
-                                                        w="100%"
-                                                        onClick={() => onToggleRoute({ route, color, name })}
-                                                    >
-                                                        <Center h="40px" w="40px" bg={color} borderRadius="md">
-                                                            <Text color="white" fontWeight="bold">
-                                                                {route}
-                                                            </Text>
-                                                        </Center>
-                                                        <Text px="4" isTruncated fontWeight={500}>
-                                                            {name}
-                                                        </Text>
-                                                    </Flex>
-                                                    <Flex alignItems="center" pl="2">
-                                                        {routes[route] && (
-                                                            <Icon fontSize="2xl" mr="3">
-                                                                <CheckIcon />
-                                                            </Icon>
-                                                        )}
-
-                                                        <FavoriteIcon
-                                                            ariaLabel="fav-route"
-                                                            onClick={() => unSaveRoute(route)}
-                                                            isFav
-                                                        />
-                                                    </Flex>
-                                                </Flex>
-                                                <Divider />
-                                            </Box>
-                                        );
-                                    })}
-                                </TabPanel>
-                            </TabPanels>
-                        </Tabs>
-                    </DrawerBody>
-
-                    <DrawerFooter></DrawerFooter>
-                </DrawerContent>
-            </Drawer> */}
-        </>
+                                            <FavoriteIcon
+                                                ariaLabel="fav-route"
+                                                onClick={() => unSaveRoute(route)}
+                                                isFav
+                                            />
+                                        </Flex>
+                                    </Flex>
+                                    <Divider />
+                                </Box>
+                            );
+                        })}
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+        </BasePage>
     );
 };
--- web-app/src/utils/BasePage.tsx
+++ web-app/src/utils/BasePage.tsx
@@ -1,63 +1,19 @@
 import { FunctionComponent, ReactNode } from 'react';
 
-import { Box, Container, Flex, Button, useColorModeValue, Text, Avatar, Divider, HStack, Link } from '@chakra-ui/react';
-import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
-
-import { Info } from 'info/Info';
-import { ArrowRightIcon } from 'utils/Icons';
+import { Container, Text } from '@chakra-ui/react';
 
 interface Props {
+    title: string;
     children: ReactNode;
 }
 
-export const BasePage: FunctionComponent<Props> = ({ children }) => {
-    const { t } = useTranslation();
-    const navigate = useNavigate();
-
+export const BasePage: FunctionComponent<Props> = ({ children, title }) => {
     return (
-        <Container maxW="container.lg" p="0">
-            <Flex
-                zIndex={100}
-                position="sticky"
-                top="0"
-                justifyContent="space-between"
-                alignItems="center"
-                mb="6"
-                p="4"
-                backgroundColor={useColorModeValue('white', 'gray.800')}
-            >
-                <HStack alignItems="center" spacing={8} display={{ base: 'none', md: 'flex' }}>
-                    <Avatar src="/logo.svg" h="40px" w="40px" ignoreFallback />
-                    <Link fontWeight="bold" pl="4">
-                        Usage
-                    </Link>
-                    <Link fontWeight="bold">Contact</Link>
-                    <Link fontWeight="bold">Legal</Link>
-                    <Link fontWeight="bold">Settings</Link>
-                </HStack>
-
-                <Box display={{ base: 'block', md: 'none' }}>
-                    <Info disableAvatarShadow />
-                </Box>
-
-                <Button colorScheme="blue" rightIcon={<ArrowRightIcon />} onClick={() => navigate('/')}>
-                    {t('START_TRACKING')}
-                </Button>
-            </Flex>
-            <Container maxW="container.lg">{children}</Container>
-            <Divider mt="12" />
-            <Flex justifyContent="space-between" alignItems="center" p="4" flexDir={{ base: 'column', md: 'row' }}>
-                <Flex alignItems="center">
-                    <Avatar src="/logo.svg" size="sm" ignoreFallback />
-                    <Text pl="2" fontWeight={800}>
-                        trackCTA
-                    </Text>
-                </Flex>
-                <Text fontSize="xs" pt={{ base: '4', md: '0' }}>
-                    © {new Date().getFullYear()} trackCTA. All rights reserved.
-                </Text>
-            </Flex>
+        <Container maxW="container.sm" p="0" py="4">
+            <Text fontWeight="bold" fontSize="4xl" px="4">
+                {title}
+            </Text>
+            {children}
         </Container>
     );
 };
